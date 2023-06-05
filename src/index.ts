a6bc010fa831d714d8a29ba39b941e8e1be5a4d2
ignacio.lima <ignacio.lima@inswitch.com>
2023-06-05 20:50:01 -0300

    Agrego el endpoint para cargar las tarjetas de un usuario

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,28 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
+const users = [
+  {
+    userName: "joseechague",
+    cards: [
+      {
+        id: 1,
+        title: "Subir lecturas",
+        description: "Subir 200 paginas para leer 1 dia antes del RAT",
+        selectedMovieId: 100,
+        movieImageUrl: "http://localhost.com",
+      },
+    ],
+  },
+];
+
+app.get("/users/:userName/cards", (req, res) => {
+  const { userName } = req.params;
+  const user = users.find((u) => u.userName === userName);
+  if (!user) {
+    res.status(404).send("User cards not found");
+  }
+  res.send(user?.cards);
 });
 
-app.listen(port, () => {
-  // console.log("Running on port: " + port);
-});
+app.listen(port);
