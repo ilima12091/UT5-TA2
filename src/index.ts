9d7af1716a0e6652c15ec28c1148de0e0daff7a1
ignacio.lima <ignacio.lima@inswitch.com>
2023-06-05 20:35:50 -0300

    Agrego librerias base

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-  // console.log(`[server]: Server is running at http://localhost:${port}`);
+  // console.log("Running on port: " + port);
 });
